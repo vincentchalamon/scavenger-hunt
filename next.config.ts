9522fea82ab405f347944015714d710ee5334f7a
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-08-25 14:37:31 +0200

    chore: init PWA with create-next-app

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // output: 'export',
+  // trailingSlash: true,
+  // skipTrailingSlashRedirects: true,
+  // distDir: 'docs',
+};
+
+export default nextConfig;
